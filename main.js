18bed588193d665f584fb5b83768c28c4367749d
Bob Holben <bholben@gmail.com>
2015-01-20 15:17:28 -0500

    Add full toggle functionality

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 
+// // Basic approach (does not allow closing of the clicked section)
+// $('.title').on('click', function () {
+//   $('.content').addClass('collapse');
+//   $(this).parent().children('.content').removeClass('collapse');
+// });
+
+
+// Refined approach with full toggle functionality allows
+// repeated open & close of the clicked section.
 $('.title').on('click', function () {
-  $('.content').addClass('collapse');
-  $(this).parent().children('.content').removeClass('collapse');
+  var thisContent = $(this).parent().children('.content'),
+      allContent = $('.content');
+
+  if (thisContent.hasClass('collapse')) {
+    allContent.addClass('collapse');
+    thisContent.removeClass('collapse');
+  } else {
+    allContent.addClass('collapse');
+  }
 });
 
